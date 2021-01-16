4355135bbf3a96fcc63a49676e9be61b4c14cb06
vnikonov63 <nikonov.va@yandex.ru>
2021-01-16 04:46:49 +0400

    Added the functionality to work without set

--- 15.3Sum/15.3Sum/main.cpp
+++ 15.3Sum/15.3Sum/main.cpp
@@ -6,7 +6,38 @@
 
 using namespace std;
 
+
 vector<vector<int>> threeSum(vector<int>& nums) {
+    if (nums.size() < 3) {
+        return {};
+    }
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> result;
+    
+    for (int i = 0; i < nums.size() - 2; ++i) {
+        if (i == 0 || nums[i] != nums[i - 1]) {
+            int j = i + 1;
+            int k = static_cast<int>(nums.size()) - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.push_back({nums[i], nums[j++], nums[k--]});
+                    while (j < k && nums[j] == nums[j-1]) {
+                        j++;
+                    }
+                }
+            }
+        }
+    }
+    
+    return result;
+}
+
+vector<vector<int>> threeSumSet(vector<int>& nums) {
         if (nums.size() < 3) {
             return {};
         }
@@ -14,7 +45,7 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 2; ++i) {
             int j = i + 1;
-            int k = nums.size() - 1;
+            int k = static_cast<int>(nums.size()) - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum < 0) {
@@ -35,6 +66,7 @@ void Test() {
     
     vector<vector<int>> expected1 = {{-1,-1,2},{-1,0,1}};
     
+    ASSERT_EQUAL(threeSumSet(input1), expected1);
     ASSERT_EQUAL(threeSum(input1), expected1);
 }
 
